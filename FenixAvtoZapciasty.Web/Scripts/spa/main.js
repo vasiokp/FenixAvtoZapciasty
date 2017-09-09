523046f54a419dc433404a16eff39e4938d15d4f
Vasyl Poklitar <vasiok26@gmail.com>
2017-09-09 09:49:43 +0300

    Added ui-routing

---
+++ FenixAvtoZapciasty.Web/Scripts/spa/main.js
@@ -0,0 +1,54 @@
+﻿'use strict'
+console.log(213)
+var app = angular.module('myApp', ['ui.router'])
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('app', {
+				url: '/',
+				views: {
+					'navbar': {
+						templateUrl: 'Scripts/spa/navbar/navbar.html',
+					},
+					'menu': {
+						templateUrl: 'Scripts/spa/menu/menu.html',
+					},
+					'content': {
+						templateUrl: 'Scripts/spa/content/content.html',
+						//controller: 'contentController'
+					},
+					'footer': {
+						templateUrl: 'Scripts/spa/footer/footer.html',
+					}
+				}
+			})
+			.state('app.about', {
+				url: 'about',
+				views: {
+					'content@': {
+						templateUrl: 'assets/views/about.html',
+						controller: 'formController'
+					}
+				}
+			})
+			.state('app.addpage', {
+				url: 'addpage',
+				views: {
+					'content@': {
+						templateUrl: 'assets/views/addpage.html',
+						controller: 'formController'
+					}
+				}
+			})
+			.state('app.info', {
+				url: 'info/:id',
+				views: {
+					'content@': {
+						templateUrl: 'assets/views/info.html',
+						controller: 'infoController'
+					}
+				}
+			});
+
+		$urlRouterProvider.otherwise('/');
+
+	});
